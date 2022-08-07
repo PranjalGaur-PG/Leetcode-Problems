0f6d704471820f8ad30d992ac7a46dc09b67f33a
Pranjal Gaur <60785715+PranjalGaur-PG@users.noreply.github.com>
2022-08-07 23:55:16 +0530

    Time: 7 ms (79.55%), Space: 8.7 MB (66.85%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& tri) {
-//         int n = tri.size();
+        int n = tri.size();
+        if(n==1) return tri[0][0];
         
-//         for(int i=1;i<n;i++) {
-//             for(int j=0;j<tri[i].size();j++) {
+        int ans = INT_MAX;
+        for(int i=1;i<n;i++) {
+            for(int j=0;j<=i;j++) {
+                if(j==0) tri[i][j] += tri[i-1][j];
+                else if(j==i) tri[i][j] += tri[i-1][j-1];
+                else tri[i][j] = tri[i][j] + min(tri[i-1][j], tri[i-1][j-1]);
                 
-//                 if(j>0) tri[i][j] += min(tri[i-1][j], tri[i-1][j-1]);
-//                 else tri[i][j] += tri[i-1][j];
-            
-//             }
-//         }
+                if(i==n-1) ans = min(ans, tri[i][j]);
+            }
+        }
         
-//         int ans = INT_MAX;
-//         for(int i=0;i<tri[n-1].size();i++) ans = min(ans, tri[n-1][i]);
-//         return ans;
-            vector<int> res = tri.back();
-    for (int i = tri.size()-2; i >= 0; i--) 
-        for (unsigned int j = 0; j <= i; j++) 
-            res[j] = tri[i][j] + min(res[j], res[j+1]);
-    return res[0];
-
+        return ans;
     }
 };
