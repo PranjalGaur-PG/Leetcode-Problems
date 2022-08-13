719fbb00ceb03b5a0ea55055a8a33ff74a2de869
Pranjal Gaur <60785715+PranjalGaur-PG@users.noreply.github.com>
2022-08-13 20:56:53 +0530

    Time: 135 ms (57.61%), Space: 45.8 MB (20.46%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amt) {
-        vector<int> dp(amt+1, amt+1);
-        dp[0] = 0;
-        for(int i=1;i<=amt;i++) {
-            for(int j=0;j<coins.size();j++) {
-                if(i-coins[j] >= 0) {
-                    dp[i] = min(dp[i], 1 + dp[i-coins[j]]);
-                }
-            }
+    int rec(vector<int>& coins, int amt, int ind, vector<vector<int>>& dp) {
+        
+        if(amt == 0) return 0;
+        if(ind == 0) {
+            if(amt%coins[ind] == 0) return amt/coins[ind];
+            else return 1e6;
         }
-        if(dp[amt] == amt+1) return -1;
-        return dp[amt];
+        
+        if(dp[ind][amt] != -1) return dp[ind][amt];
+        
+        int nt = rec(coins, amt, ind-1, dp);
+        int t = 1e6;
+        if(coins[ind] <= amt) t = 1 + rec(coins, amt-coins[ind], ind, dp);
+        
+        return dp[ind][amt] = min(t, nt);
+    }
+        
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        
+        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+        int ans = rec(coins, amount, n-1, dp);
+        
+        if(ans >= 1e6) return -1;
+        return ans;
     }
 };
