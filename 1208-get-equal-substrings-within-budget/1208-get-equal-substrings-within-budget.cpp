b2da4841c2c6506d20f8b8471de7abfa9184d6f1
Pranjal Gaur <60785715+PranjalGaur-PG@users.noreply.github.com>
2022-09-22 21:07:42 +0530

    Time: 26 ms (9.55%), Space: 8.4 MB (13.38%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -19,30 +19,30 @@ public:
         vector<int> amt(n);
         for(int i=0;i<n;i++) amt[i] = abs(s[i]-t[i]);
         
-        int i=0, j=0, temp=0, ans=0;
-        while(j<n) {
-            temp += amt[j];
-            while(temp > maxCost) temp -= amt[i++];
+//         int i=0, j=0, temp=0, ans=0;
+//         while(j<n) {
+//             temp += amt[j];
+//             while(temp > maxCost) temp -= amt[i++];
             
-            ans = max(ans, j-i+1);
-            j++;
-        }
-        
-        return ans;
-        
-//         cout<<n;
-        
-//         int i = 0, j = n-1, ans = 0;
-        
-//         while(i<=j) {
-//             int mid = i + (j-i)/2;
-//             if(solve(amt, mid, maxCost, n)) {
-//                 ans = max(mid, ans);
-//                 i = mid+1;
-//             }
-//             else j = mid-1;
+//             ans = max(ans, j-i+1);
+//             j++;
 //         }
         
 //         return ans;
+        
+//         cout<<n;
+        
+        int i = 1, j = n, ans = 0;
+        
+        while(i<=j) {
+            int mid = i + (j-i)/2;
+            if(solve(amt, mid, maxCost, n)) {
+                ans = max(mid, ans);
+                i = mid+1;
+            }
+            else j = mid-1;
+        }
+        
+        return ans;
     }
 };
