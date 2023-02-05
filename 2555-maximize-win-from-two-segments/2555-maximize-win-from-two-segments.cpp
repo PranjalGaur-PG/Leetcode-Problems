235422f0a081d9688426369f6254d7830a7c53e5
Pranjal Gaur <60785715+PranjalGaur-PG@users.noreply.github.com>
2023-02-05 10:09:09 +0530

    Time: 132 ms (100.00%), Space: 59.1 MB (100.00%) - LeetHub

--- 2555-maximize-win-from-two-segments/2555-maximize-win-from-two-segments.cpp
+++ 2555-maximize-win-from-two-segments/2555-maximize-win-from-two-segments.cpp
@@ -18,16 +18,15 @@ public:
             mx = max(i-j+1, mx);
             bk[j] = mx;
             while(j<=i && arr[i]-arr[j] > k) i--;
-            // bk[j] = i-j+1;
             j--;
         }
         
         for(int i=0;i<n;i++) {
-            // cout<<fr[i]<<" "<<bk[i]<<endl;
-            if(fr[i]==1 && bk[i]==1) ans = max(ans, 2);
-            else ans = max(ans, fr[i]+bk[i]-1);
+            // if(fr[i]==1 && bk[i]==1) ans = max(ans, 2);
+            // else
+                ans = max(ans, fr[i]+bk[i]-1);
         }
-        // cout<<endl;
+        
         return ans;
         
     }
