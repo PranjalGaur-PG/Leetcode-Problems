4976087e8461c78b12b3e7d38a0d0a83f6eb5077
Pranjal Gaur <60785715+PranjalGaur-PG@users.noreply.github.com>
2023-03-26 17:15:44 +0530

    Time: 1164 ms (13.34%), Space: 117.2 MB (11.62%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
-    vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
-        priority_queue<pair<double, pair<int,int>>>pq;
-        for(int i = 0; i < A.size(); i++)
-            pq.push({-1.0*A[i]/A.back(), {i,A.size()-1}});
+    vector<int> kthSmallestPrimeFraction(vector<int>& A, int k) {
+//         priority_queue<pair<double, pair<int,int>>>pq;
+//         for(int i = 0; i < A.size(); i++)
+//         {
+//             for(int j=i+1;j<A.size();j++) {
+//                 pq.push({1.0*A[i]/A[j], {A[i],A[j]}});
+//             }
+            
+//         }
         
-        while(--K > 0)
-        {
-            pair<int,int> cur = pq.top().second;
-            pq.pop();
-            cur.second--;
-            pq.push({-1.0*A[cur.first]/A[cur.second], {cur.first, cur.second}});
+//        while(k--) pq.pop();
+        
+//         vector<int> ans;
+//         ans.push_back(pq.top().second.first);
+//         ans.push_back(pq.top().second.second);
+//         return ans;
+        priority_queue<pair<double,pair<int,int>>> Q;
+        
+        for(int i=0;i<A.size();i++){
+            for(int j=A.size()-1;j>i;j--){
+                double a = (double)A[i]/(double)A[j];
+                Q.push({a,{A[i],A[j]}});
+            }
         }
-        return {A[pq.top().second.first], A[pq.top().second.second]};
         
+        int n = Q.size();
+        while(n>k){
+        Q.pop();
+        n--;
+        }
+        auto a = Q.top();
+        vector<int> ans(2);
+        ans[0] = a.second.first;
+        ans[1] = a.second.second;
+        return ans;
+
     }
 };
