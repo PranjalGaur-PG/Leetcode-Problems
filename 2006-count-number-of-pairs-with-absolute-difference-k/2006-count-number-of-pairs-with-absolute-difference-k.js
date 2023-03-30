9a125a62ae68d4f60a1678c7452ac563406af34d
Pranjal Gaur <60785715+PranjalGaur-PG@users.noreply.github.com>
2023-03-30 11:27:31 +0530

    Time: 68 ms (61.86%), Space: 42.6 MB (57.94%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countKDifference = function(nums, k) {
+    let n = nums.length;
+    
+    let cnt = 0;
+    for(let i=0;i<n;i++) {
+        for(let j=i+1;j<n;j++) {
+            if(Math.abs(nums[i]-nums[j]) == k) cnt++;
+        }
+    }
+    
+    return cnt;
+};
